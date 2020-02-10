12fe16bce418d56a6b7815ca0f717af924c8d437
Effi Ban <effi.ban@kenshoo.com>
2020-02-10 19:29:04 +0200

    Add unit tests

--- main/src/main/java/com/kenshoo/pl/entity/PLCondition.java
+++ main/src/main/java/com/kenshoo/pl/entity/PLCondition.java
@@ -2,6 +2,9 @@ package com.kenshoo.pl.entity;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.jooq.Condition;
 
 import java.util.Set;
@@ -46,5 +49,35 @@ public class PLCondition {
         requireNonNull(condition, "a condition must be provided");
         return new PLCondition(condition.jooqCondition.not(),
                                condition.fields);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PLCondition that = (PLCondition) o;
+
+        return new EqualsBuilder()
+            .append(jooqCondition, that.jooqCondition)
+            .append(fields, that.fields)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+            .append(jooqCondition)
+            .append(fields)
+            .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("jooqCondition", jooqCondition)
+            .append("fields", fields)
+            .toString();
     }
 }
---
+++ main/src/test/java/com/kenshoo/pl/entity/EntityFieldConditionsTest.java
@@ -0,0 +1,78 @@
+package com.kenshoo.pl.entity;
+
+import com.google.common.collect.ImmutableSet;
+import com.kenshoo.jooq.AbstractDataTable;
+import com.kenshoo.jooq.DataTable;
+import org.jooq.Record;
+import org.jooq.TableField;
+import org.jooq.impl.SQLDataType;
+import org.junit.Test;
+
+import java.util.Objects;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class EntityFieldConditionsTest {
+
+    @Test
+    public void eqForRegularFieldShouldBuildConditionWithField() {
+        final PLCondition plCondition = TestEntityType.NAME1.eq("abc");
+
+        assertContainsFields(plCondition, TestEntityType.NAME1);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void eqForVirtualFieldShouldThrowException() {
+        TestEntityType.FULL_NAME.eq("abc");
+    }
+
+    private void assertContainsFields(final PLCondition plCondition,
+                                      final EntityField<?, ?>... fields) {
+        assertThat("Incorrect fields in PL condition: ",
+                   plCondition.getFields(), equalTo(ImmutableSet.copyOf(fields)));
+    }
+
+    private static class TestTable extends AbstractDataTable<TestTable> {
+        private static final TestTable INSTANCE = new TestTable("test");
+
+        private final TableField<Record, Integer> id = createPKField("id", SQLDataType.INTEGER);
+        private final TableField<Record, String> name1 = createField("name1", SQLDataType.VARCHAR.length(50));
+        private final TableField<Record, String> name2 = createField("name2", SQLDataType.VARCHAR.length(50));
+
+        public TestTable(String name) {
+            super(name);
+        }
+
+        public TestTable(TestTable aliased, String alias) {
+            super(aliased, alias);
+        }
+
+        @Override
+        public TestTable as(String alias) {
+            return new TestTable(this, alias);
+        }
+    }
+
+    public static class TestEntityType extends AbstractEntityType<TestEntityType> {
+
+        public static final TestEntityType INSTANCE = new TestEntityType();
+
+        public static final EntityField<TestEntityType, Integer> ID = INSTANCE.field(TestTable.INSTANCE.id);
+        public static final EntityField<TestEntityType, String> NAME1 = INSTANCE.field(TestTable.INSTANCE.name1);
+        public static final EntityField<TestEntityType, String> NAME2 = INSTANCE.field(TestTable.INSTANCE.name2);
+        public static final EntityField<TestEntityType, String> FULL_NAME = INSTANCE.virtualField(NAME1, NAME2,
+                                                                                                  (name1, name2) -> name1 + " " + name2,
+                                                                                                  new CommonTypesStringConverter<>(String.class),
+                                                                                                  Objects::equals);
+
+        private TestEntityType() {
+            super("test");
+        }
+
+        @Override
+        public DataTable getPrimaryTable() {
+            return TestTable.INSTANCE;
+        }
+    }
+}
