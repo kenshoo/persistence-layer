aec8a254a4720e71d71877709204d6a2439d7843
Effi Ban <effi.ban@kenshoo.com>
2020-03-31 15:06:39 +0300

    Support the LocalDate type for table fields

--- main/src/main/java/com/kenshoo/pl/entity/CommonTypesStringConverter.java
+++ main/src/main/java/com/kenshoo/pl/entity/CommonTypesStringConverter.java
@@ -6,6 +6,8 @@ import org.apache.commons.lang3.BooleanUtils;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
@@ -20,9 +22,12 @@ import java.util.function.Function;
  */
 public class CommonTypesStringConverter<T> implements ValueConverter<T, String> {
 
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
+
     private static final Map<Class<?>, Function<?, String>> TO_STRING_FUNCTION_BY_CLASS = ImmutableMap.<Class<?>, Function<?, String>>builder()
             .put(Timestamp.class, o -> Long.toString(((Timestamp) o).getTime()))
             .put(Instant.class, i -> Long.toString(((Instant) i).toEpochMilli()))
+            .put(LocalDate.class, (Function<LocalDate, String>) DATE_FORMATTER::format)
             .build();
 
     private static final Map<Class<?>, Function<String, ?>> FROM_STRING_FUNCTION_BY_CLASS = ImmutableMap.<Class<?>, Function<String, ?>>builder()
@@ -35,6 +40,7 @@ public class CommonTypesStringConverter<T> implements ValueConverter<T, String> 
             .put(BigDecimal.class, BigDecimal::new)
             .put(Timestamp.class, s -> new Timestamp(Long.parseLong(s)))
             .put(Instant.class, s -> Instant.ofEpochMilli(Long.parseLong(s)))
+            .put(LocalDate.class, s -> DATE_FORMATTER.parse(s, LocalDate::from))
             .build();
 
     private Class<T> valueClass;
