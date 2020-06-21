e045511ed38bd589f6530a2b08f0dda5030b075d
Effi Ban <effi.ban@kenshoo.com>
2020-06-21 13:55:15 +0300

    Add Entity.getOptional() to replace multiple private methods

--- main/src/main/java/com/kenshoo/pl/entity/Entity.java
+++ main/src/main/java/com/kenshoo/pl/entity/Entity.java
@@ -1,6 +1,7 @@
 package com.kenshoo.pl.entity;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 
@@ -21,6 +22,13 @@ public interface Entity {
     boolean containsField(EntityField<?, ?> field);
 
     <T> T get(EntityField<?, T> field);
+
+    default <T> Optional<T> getOptional(final EntityField<?, T> field) {
+        if (containsField(field)) {
+            return Optional.ofNullable(get(field));
+        }
+        return Optional.empty();
+    }
 
     default <E extends EntityType<E>> List<FieldsValueMap<E>> getMany(E type) {
         return emptyList();
