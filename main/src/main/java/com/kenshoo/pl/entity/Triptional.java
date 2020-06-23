26032d1fc434a226107f9f1e41d0f383586a6f2b
Effi Ban <effi.ban@kenshoo.com>
2020-06-23 14:51:13 +0300

    Renaming after discussion

--- main/src/main/java/com/kenshoo/pl/entity/Triptional.java
+++ main/src/main/java/com/kenshoo/pl/entity/Triptional.java
@@ -4,66 +4,66 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static com.kenshoo.pl.entity.TriState.State.*;
+import static com.kenshoo.pl.entity.Triptional.State.*;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Function.identity;
 
-public class TriState<T> {
+public class Triptional<T> {
 
     enum State {
         FILLED,
-        EMPTY,
+        NULL,
         ABSENT
     }
 
-    private static final TriState<?> EMPTY_INSTANCE = new TriState<>(EMPTY);
-    private static final TriState<?> ABSENT_INSTANCE = new TriState<>(ABSENT);
+    private static final Triptional<?> NULL_INSTANCE = new Triptional<>(NULL);
+    private static final Triptional<?> ABSENT_INSTANCE = new Triptional<>(ABSENT);
 
     private final T value;
     private final State state;
 
-    private TriState(final State state) {
+    private Triptional(final State state) {
         this(null, state);
     }
 
-    private TriState(final T value) {
+    private Triptional(final T value) {
         this(value,
-             value == null ? EMPTY : FILLED);
+             value == null ? NULL : FILLED);
     }
 
-    private TriState(final T value, final State state) {
+    private Triptional(final T value, final State state) {
         this.value = value;
         this.state = state;
     }
 
-    public static <T> TriState<T> of(final T value) {
-        return new TriState<>(value);
+    public static <T> Triptional<T> of(final T value) {
+        return new Triptional<>(value);
     }
 
     @SuppressWarnings("unchecked")
-    public static <T> TriState<T> empty() {
-        return (TriState<T>)EMPTY_INSTANCE;
+    public static <T> Triptional<T> nul() {
+        return (Triptional<T>) NULL_INSTANCE;
     }
 
     @SuppressWarnings("unchecked")
-    public static <T> TriState<T> absent() {
-        return (TriState<T>)ABSENT_INSTANCE;
+    public static <T> Triptional<T> absent() {
+        return (Triptional<T>)ABSENT_INSTANCE;
     }
 
-    public <U> TriState<U> map(final Function<? super T, ? extends U> mapper) {
+    public <U> Triptional<U> map(final Function<? super T, ? extends U> mapper) {
         return map(mapper, () -> null);
     }
 
-    public <U> TriState<U> map(final Function<? super T, ? extends U> filledMapper,
-                               final Supplier<? extends U> emptySupplier) {
+    public <U> Triptional<U> map(final Function<? super T, ? extends U> filledMapper,
+                                 final Supplier<? extends U> nullReplacer) {
         requireNonNull(filledMapper, "filledMapper is required");
-        requireNonNull(emptySupplier, "emptySupplier is required");
+        requireNonNull(nullReplacer, "nullReplacer is required");
 
         switch (state) {
             case FILLED:
                 return of(filledMapper.apply(value));
-            case EMPTY:
-                return of(emptySupplier.get());
+            case NULL:
+                return of(nullReplacer.get());
             default:
                 return absent();
         }
@@ -78,15 +78,23 @@ public class TriState<T> {
     }
 
     public <U> Optional<U> mapToOptional(final Function<? super T, ? extends U> filledMapper,
-                                         final Supplier<? extends U> emptySupplier) {
+                                         final Supplier<? extends U> nullReplacer) {
         switch (state) {
             case FILLED:
                 return Optional.ofNullable(filledMapper.apply(value));
-            case EMPTY:
-                return Optional.ofNullable(emptySupplier.get());
+            case NULL:
+                return Optional.ofNullable(nullReplacer.get());
             default:
                 return Optional.empty();
         }
+    }
+
+    public boolean isPresent() {
+        return !isAbsent();
+    }
+
+    public boolean isAbsent() {
+        return state == ABSENT;
     }
 
     public boolean isFilled() {
@@ -97,11 +105,7 @@ public class TriState<T> {
         return !isFilled();
     }
 
-    public boolean isEmpty() {
-        return state == EMPTY;
-    }
-
-    public boolean isAbsent() {
-        return state == ABSENT;
+    public boolean isNull() {
+        return state == NULL;
     }
 }
