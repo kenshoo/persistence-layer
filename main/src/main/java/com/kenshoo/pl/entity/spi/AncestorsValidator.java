9ed87893030df34ac9aef11d5d5602411a9a1fb5
dimagorelik <dima.gorelik@kenshoo.com>
2020-07-16 09:55:52 +0300

    Fix java doc

--- main/src/main/java/com/kenshoo/pl/entity/spi/AncestorsValidator.java
+++ main/src/main/java/com/kenshoo/pl/entity/spi/AncestorsValidator.java
@@ -8,14 +8,14 @@ import java.util.stream.Stream;
 public interface AncestorsValidator<E extends EntityType<E>> extends ChangeValidator {
 
     /**
-     * @return the fields of the parent entities whose value is required for the validation
+     * @return the fields of the ancestor entities whose value is required for the validation
      */
     Stream<EntityField<?, ?>> ancestorsFields();
 
     /**
      * Called by the framework to check if the change is restricted by ancestors. The implementation can query the <code>entity</code> only for
      * the fields it has declared in {@link #ancestorsFields()}.
-     * @return Predicate when parent immutable.
+     * @return Predicate when restricted by ancestors.
      */
     default Predicate<Entity> ancestorsRestriction() {
         return e -> true;
