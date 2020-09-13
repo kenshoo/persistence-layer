330d8865c05cdf1001e5e5199b9b173ebcfae640
Dima Gorelik <dima.gorelik@kenshoo.com>
2020-09-13 11:27:51 +0300

    Delete deprecated parent condition validator

--- main/src/main/java/com/kenshoo/pl/entity/spi/helpers/CompoundChangesValidatorFactory.java
+++ main/src/main/java/com/kenshoo/pl/entity/spi/helpers/CompoundChangesValidatorFactory.java
@@ -4,17 +4,14 @@ import com.kenshoo.pl.entity.EntityType;
 import com.kenshoo.pl.entity.internal.validators.EntityChangeValidator;
 import com.kenshoo.pl.entity.spi.ChangeValidator;
 import com.kenshoo.pl.entity.spi.ChangesValidator;
-import com.kenshoo.pl.entity.spi.ParentConditionValidator;
 
 import java.util.Collection;
 
 public class CompoundChangesValidatorFactory {
 
     public static <E extends EntityType<E>> ChangesValidator<E> buildChangesValidator(E entityType, Collection<?> validators) {
-        ParentConditionCompositeValidator<E> parentConditionsValidator = new ParentConditionCompositeValidator<>();
         EntityChangeCompositeValidator<E> entityChangeValidator = new EntityChangeCompositeValidator<>();
         CompoundChangesValidator<E> compoundChangesValidator = new CompoundChangesValidator<>();
-        compoundChangesValidator.register(parentConditionsValidator);
         compoundChangesValidator.register(entityChangeValidator);
 
         for (Object validator : validators) {
@@ -23,8 +20,6 @@ public class CompoundChangesValidatorFactory {
             } else if(validator instanceof EntityChangeValidator) {
                 //noinspection unchecked
                 entityChangeValidator.register((EntityChangeValidator<E>)validator);
-            } else if (validator instanceof ParentConditionValidator) {
-                parentConditionsValidator.register((ParentConditionValidator) validator);
             } else if (validator instanceof ChangesValidator) {
                 //noinspection unchecked
                 compoundChangesValidator.register((ChangesValidator<E>) validator);
